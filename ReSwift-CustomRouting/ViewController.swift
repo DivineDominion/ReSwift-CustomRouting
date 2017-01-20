c53aa6c4ffccd088f5341cd78306d44fd9c655ec
Christian Tietze <christian.tietze@gmail.com>
2017-01-20 14:40:36 +0100

    Initial Commit

---
+++ ReSwift-CustomRouting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReSwift-CustomRouting
+//
+//  Created by Christian Tietze on 20/01/17.
+//  Copyright © 2017 ReSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
